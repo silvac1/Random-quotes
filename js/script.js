0e532028c08c76897d92748f60136b17b658f32a
Celso Silva <silva.celso29@gmail.com>
2019-03-20 10:09:37 -0400

    refactored the code

--- js/script.js
+++ js/script.js
@@ -1,67 +1,34 @@
 /******************************************
-Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+//This is my array of objects containing all the Quotes
 
 let quotes = [
   { quote: "I'm so fly, when i driveby!", source: "Celso" },
   { quote: "Bum, bum bum ..bum ..bum", source: "Salman", citation: 'Twitter', year: 2000 },
   { quote: "On top of the world", source: "Celso" },
   { quote: "Night time takes so long", source: "Muhammad" },
-  { quote: "Baba", source: "jannaat", citation: "Github", year: 1990 },
-  { quote: "Honey, your so sweet", source: "Majida", citation: "WhatsApp", year: 2019 }
+  { quote: "Baba", source: "jannaat", citation: "Facetime", year: 1990 },
+  { quote: "Honey, your so sweet", source: "Majida", citation: "WhatsApp", year: 2019 },
+  { quote: "Nothing is impossible, the word itself says “I’m possible”!", source: " Audrey Hepburn" },
+  { quote: "Perfection is not attainable, but if we chase perfection we can catch excellence.", source: "Vince Lombardi", citation: 'Google', year: 2019 },
+  { quote: "I am not a product of my circumstances. I am a product of my decisions.", source: "Stephen Covey" },
+  { quote: "The most difficult thing is the decision to act, the rest is merely tenacity.", source: "Amelia Earhart" },
+  { quote: "The quickest way to double your money is to fold it over and put it back in your pocket.", source: "Will Rogers", citation: "Facebook", year: 2017 },
+  { quote: "The only person you are destined to become is the person you decide to be.", source: "Ralph Waldo Emerson", citation: "WhatsApp", year: 2019 }
 ];
 
-//console.log(quotes);
+//Will return a random quote selection
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
 function getRandomQuote() {
     let i = Math.floor(Math.random() * quotes.length);
     let ranNum = quotes[i];
     return ranNum;
 }
-//console.log(getRandomQuote());
 
-
-
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-
-
-
-
-
-// function print(message) {
-//   let qouteDiv = document.getElementById('quote-box');
-//       qouteDiv.innerHTML = message;
-// }
-
+//Produces and random quote and in new html format while checking if specific property exists
 
 function printQuote() {
   var ranQuote = getRandomQuote();
@@ -77,15 +44,6 @@ function printQuote() {
      document.getElementById('quote-box').innerHTML = htmlString;
 }
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
+//event addEventListener on the show another qoute button
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
